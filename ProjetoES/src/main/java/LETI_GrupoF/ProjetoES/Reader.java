36c56ac4f1db8d214a60a5b27db7a937d16cb9ed
ralso2 <ralso2@iscte-iul.pt>
2023-11-04 18:59:34 +0000

    Reestruturação do Reader
    Incompleto

--- ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Reader.java
+++ ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Reader.java
@@ -1,5 +1,42 @@
 package LETI_GrupoF.ProjetoES;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Reader {
-    
+
+	File ficheiroCSV = new File("HorarioDeExemplo.csv");
+	String[][] tableData = new String[10][10];
+
+	public String[][] dividirPorColuna() {
+		String nextLine;
+		Scanner sc;
+		try {
+			sc = new Scanner(ficheiroCSV);
+			if (sc.hasNextLine()) // ignora a primeira linha
+				sc.nextLine();
+
+			int i = 0;
+			while (sc.hasNextLine()) {
+				nextLine = sc.nextLine();
+				String[] line = nextLine.split(";");
+				tableData[i] = line;
+
+				System.out.println(tableData[i]);
+				System.out.println("");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return tableData;
+
+	}
+
+	public static void main(String[] args) {
+		Reader a = new Reader();
+		a.dividirPorColuna();
+	}
+
 }
