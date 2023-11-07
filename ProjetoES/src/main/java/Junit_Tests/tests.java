e07b8f2f02f8281d0c0a98f04a1ab2441f5bb79c
jpnoa1 <145027688+jpnoa1@users.noreply.github.com>
2023-11-07 10:51:49 +0000

    Test Creation
    testes de caixa branca(por implementar)

---
+++ ProjetoES/src/main/java/Junit_Tests/tests.java
@@ -0,0 +1,26 @@
+package Junit_Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class tests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	
+	
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
