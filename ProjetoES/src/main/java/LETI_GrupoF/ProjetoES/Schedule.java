364b772838a79427bfeba0c9b2e09e244c4e84d9
jpnoa1 <jpnoa1@iscte-iul.pt>
2023-11-25 17:01:15 +0000

    Comentarios para JavaDoc para classe schedule

--- ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Schedule.java
+++ ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Schedule.java
@@ -3,10 +3,21 @@ package LETI_GrupoF.ProjetoES;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Schedule representa um horario especifico para uma determinada
+ * turma.
+ */
 public class Schedule {
 	private String turma;
 	private Reader table;
 	private List<List<String>> horario = new ArrayList<>();;
+
+	/**
+	 * Construtor da classe Schedule.
+	 *
+	 * @param turma, turma para a qual o horario esta sendo gerado.
+	 * @param table, Reader de tabela que fornece os dados para o horario.
+	 */
 
 	public Schedule(String turma, Reader table) {
 		this.turma = turma;
@@ -23,6 +34,15 @@ public class Schedule {
 
 	}
 
+	/**
+	 * Verifica se uma determinada turma existe em uma linha especifica da tabela.
+	 *
+	 * @param linhaTurma A string que contem mais que uma turma ou nao de uma dada
+	 *                   linha.
+	 * @param turma      A turma a ser verificada.
+	 * @return true se a turma existir na linha, false caso contrario.
+	 */
+
 	private boolean existeTurma(String linhaTurma, String turma) {
 		String[] turmas = linhaTurma.split(",");
 		for (int i = 0; i != turmas.length; i++) {
@@ -31,6 +51,12 @@ public class Schedule {
 		}
 		return false;
 	}
+
+	/**
+	 * Obtém o índice da coluna "Turma" na tabela.
+	 *
+	 * @return O índice da coluna "Turma".
+	 */
 
 	public int indiceColunaTurma() {
 		int columnNumber = 0;
@@ -44,6 +70,13 @@ public class Schedule {
 		return columnNumber;
 	}
 
+	/**
+	 * Verifica se uma turma específica existe no horário.
+	 *
+	 * @param linha A string que contem informações sobre a turma.
+	 * @return true se a turma existir no horário, false caso contrário.
+	 */
+
 	boolean existsTurma(String linha) {
 		if (linha.equals(this.turma)) {
 
@@ -53,14 +86,29 @@ public class Schedule {
 
 	}
 
+	/**
+	 * Obtem o nome da turma associada a este horario.
+	 *
+	 * @return O nome da turma.
+	 */
 	public String getTurma() {
 		return turma;
 	}
 
+	/**
+	 * Obtem o horario associado a esta turma.
+	 *
+	 * @return O horario como uma lista de listas de strings.
+	 */
 	public List<List<String>> getHorario() {
 		return horario;
 	}
 
+	/**
+	 * Metodo main para testar a classe Schedule.
+	 *
+	 * @param args Os argumentos da linha de comando (não sao usados neste caso).
+	 */
 	public static void main(String[] args) {
 		Reader reader = new Reader("HorarioParaTestes.csv");
 
