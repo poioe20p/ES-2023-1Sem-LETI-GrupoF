21cae90c34420af8727c6ca64815eb2abae7cf0b
jpnoa1 <jpnoa1@iscte-iul.pt>
2023-11-27 22:57:40 +0000

    comentarios e testeSala(incompleto)

--- ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/user_interface/UserInteraction.java
+++ ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/user_interface/UserInteraction.java
@@ -9,136 +9,148 @@ import java.io.*;
 import java.net.URL;
 import java.util.List;
 
-//Esta class dá interatividade a pagina acedida pelo uitlizador
+/**
+ * Esta classe fornece interatividade à página acessada pelo usuário.
+ */
 public class UserInteraction {
 
-    //Pagina da GUI
-    private final UserForm userForm;
-    private HtmlCreator htmlCreator;
+	// Pagina da GUI
+	private final UserForm userForm;
+	private HtmlCreator htmlCreator;
 
-    //Para fazer correr o programa
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(UserInteraction::new);
-    }
+	/**
+	 * 
+	 * @param args
+	 */
+	// Para fazer correr o programa
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(UserInteraction::new);
+	}
 
-    //Construtor para inicializar a pagina da GUI
-    public UserInteraction() {
-        userForm = new UserForm();
-        userForm.setVisible(true);
+	 /**
+     * Construtor para inicializar a pagina GUI.
+     */
+	public UserInteraction() {
+		userForm = new UserForm();
+		userForm.setVisible(true);
 
-        //Define o comportamento do boão quando o mesmo é clicado
-        userForm.getOpenScheduleButton().addActionListener(e -> {
-                    if(userForm.isRemoteFile()) {
-                        htmlCreator = new HtmlCreator("ProjetoES/HorarioRemoto.csv", getUserOrderedColumnTitles());
-                        openSchedule();
-                    }
-                    else {
-                        htmlCreator = new HtmlCreator(userForm.getCsvFileLocationTextField().getText(), getUserOrderedColumnTitles());
-                        openSchedule();
-                    }
-                }
-        );
+		// Define o comportamento do boão quando o mesmo é clicado
+		userForm.getOpenScheduleButton().addActionListener(e -> {
+			if (userForm.isRemoteFile()) {
+				htmlCreator = new HtmlCreator("ProjetoES/HorarioRemoto.csv", getUserOrderedColumnTitles());
+				openSchedule();
+			} else {
+				htmlCreator = new HtmlCreator(userForm.getCsvFileLocationTextField().getText(),
+						getUserOrderedColumnTitles());
+				openSchedule();
+			}
+		});
 
-        userForm.getContinueButton().addActionListener( e -> {
-                    //Vai buscar o input do utilizador
-                    String input = userForm.getCsvFileLocationTextField().getText();
+		userForm.getContinueButton().addActionListener(e -> {
+			// Vai buscar o input do utilizador
+			String input = userForm.getCsvFileLocationTextField().getText();
 
-                    if(input != null && !input.isEmpty()) {
+			if (input != null && !input.isEmpty()) {
 
-                        //Verifica se o input é um URL ou um caminho para um ficheiro
-                        if( input.matches("^https?://.*")) {
-                            userForm.setRemoteFile(true);
-                            try {
-                                URL remoteFile = new URL(input);
-                                if(saveToLocalFile(remoteFile.openStream(), "ProjetoES/HorarioRemoto.csv")) {
-                                    Reader reader = new Reader("ProjetoES/HorarioRemoto.csv");
-                                    userForm.openReorderPage(reader.getColumnTitles());
-                                }
-                                else {
-                                    JOptionPane.showMessageDialog(userForm, "Error processing remote file, please try again", "Error", JOptionPane.ERROR_MESSAGE);
-                                }
-                            } catch (IOException ex) {
-                                throw new RuntimeException(ex);
-                            }
-                        }
-                        //Neste caso o input é um caminho para um ficheiro local
-                        else {
-                            File file = new File(input);
-                            if (file.exists()) {
-                                //Se existir usa o csv para gerar os dados para a pagina HTML e depois abre a pagina
-                                Reader reader = new Reader(input);
-                                userForm.openReorderPage(reader.getColumnTitles());
-                            } else {
-                                //No caso de não existir o ficheiro aparece uma mensagem de erro
-                                JOptionPane.showMessageDialog(userForm, "File does not exist: " + file, "Error", JOptionPane.ERROR_MESSAGE);
-                            }
-                        }
-                    } else {
-                        //No caso de não existir o ficheiro aparece uma mensagem de erro
-                        JOptionPane.showMessageDialog(userForm, "Invalid file path.", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
-        );
+				// Verifica se o input é um URL ou um caminho para um ficheiro
+				if (input.matches("^https?://.*")) {
+					userForm.setRemoteFile(true);
+					try {
+						URL remoteFile = new URL(input);
+						if (saveToLocalFile(remoteFile.openStream(), "ProjetoES/HorarioRemoto.csv")) {
+							Reader reader = new Reader("ProjetoES/HorarioRemoto.csv");
+							userForm.openReorderPage(reader.getColumnTitles());
+						} else {
+							JOptionPane.showMessageDialog(userForm, "Error processing remote file, please try again",
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+					} catch (IOException ex) {
+						throw new RuntimeException(ex);
+					}
+				}
+				// Neste caso o input é um caminho para um ficheiro local
+				else {
+					File file = new File(input);
+					if (file.exists()) {
+						// Se existir usa o csv para gerar os dados para a pagina HTML e depois abre a
+						// pagina
+						Reader reader = new Reader(input);
+						userForm.openReorderPage(reader.getColumnTitles());
+					} else {
+						// No caso de não existir o ficheiro aparece uma mensagem de erro
+						JOptionPane.showMessageDialog(userForm, "File does not exist: " + file, "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			} else {
+				// No caso de não existir o ficheiro aparece uma mensagem de erro
+				JOptionPane.showMessageDialog(userForm, "Invalid file path.", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
-    }
+	}
 
-    //Este método devolve os titulos das colunas da tabela ordenados pelo utilizador na GUI
-    public List<String> getUserOrderedColumnTitles() {
-        return userForm.getColumnTitles();
-    }
+	 /**
+     * Este metodo retorna os titulos das colunas da tabela 
+     * ordenados pelo usuario na GUI.
+     */
+	
+	public List<String> getUserOrderedColumnTitles() {
+		return userForm.getColumnTitles();
+	}
 
+	 /**
+     * Este metodo salva o arquivo remoto em um arquivo local.
+     */
+	private boolean saveToLocalFile(InputStream inputStream, String localFilePath) {
+		try {
+			File localFile = new File(localFilePath);
+			localFile.createNewFile();
 
-    //Este método guarda o ficheiro remoto para um ficheiro local
-    private boolean saveToLocalFile(InputStream inputStream, String localFilePath) {
-        try {
-            File localFile = new File(localFilePath);
-            localFile.createNewFile();
+			FileOutputStream outputStream = new FileOutputStream(localFile);
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
 
-            FileOutputStream outputStream = new FileOutputStream(localFile);
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
+			inputStream.close();
+			outputStream.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-            inputStream.close();
-            outputStream.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	/**
+     * Este metodo abre a pagina HTML com a tabela no navegador e verifica se a pagina HTML foi gerada com sucesso.
+     */
+	private void openSchedule() {
+		if (Desktop.isDesktopSupported()) {
+			Desktop desktop = Desktop.getDesktop();
+			File file = new File(htmlCreator.getHtmlPath());
+			if (file.exists()) {
+				try {
+					if (htmlCreator.generateHtmlPage()) {
+						System.out.println("HTML page generated successfully");
+						desktop.browse(file.toURI());
+					} else {
+						System.out.println("Error generating HTML page");
+					}
 
-
-    //Este método abre a pagina HTML com a tabela no browser
-    // verifica também se a pagina HTML foi gerada com sucesso
-    private void openSchedule() {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            File file = new File(htmlCreator.getHtmlPath());
-            if (file.exists()) {
-                try {
-                    if(htmlCreator.generateHtmlPage()){
-                        System.out.println("HTML page generated successfully");
-                        desktop.browse(file.toURI());
-                    } else {
-                        System.out.println("Error generating HTML page");
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                //No caso de não existir o ficheiro aparece uma mensagem de erro
-                JOptionPane.showMessageDialog(userForm, "File does not exist: " + file, "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        } else {
-            JOptionPane.showMessageDialog(userForm, "Desktop is not supported on this platform", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-
-
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				// No caso de não existir o ficheiro aparece uma mensagem de erro
+				JOptionPane.showMessageDialog(userForm, "File does not exist: " + file, "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		} else {
+			JOptionPane.showMessageDialog(userForm, "Desktop is not supported on this platform", "Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
 
 }
--- ProjetoES/src/test/java/LETI_GrupoF/ProjetoES/TestSala.java
+++ ProjetoES/src/test/java/LETI_GrupoF/ProjetoES/TestSala.java
@@ -2,19 +2,33 @@ package LETI_GrupoF.ProjetoES;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class TestSala {
-
+	
+	static String edificio1 = "d";
+	static String nome = "d";
+	static int capacidadeN = 100;
+	static int capacidadeE = 30;
+	static List<String> tipo;
+	static List<String> columnTitles;
+	static File ficheiroCSV = new File("HorarioParaTestes.csv");
+	static String nomeFicheiroCSV = "HorarioParaTestes.csv";
+	static Sala sala1;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		 sala1= new Sala(edificio1,nome,capacidadeN,capacidadeE,tipo);
 	
 	}
 
 	@Test
-	void test() {
-		
+	void testSala() {
+		sala1.lerSalasDoCSV(ficheiroCSV);
+
 	}
 
 }
