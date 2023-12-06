f2b2c8b13e8fe0c3ce8f11b99e7a3523c5b69684
ralso2 <ralso2@iscte-iul.pt>
2023-12-06 22:29:33 +0000

    Create SaveState.java

--- ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Horario.java
+++ ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/Horario.java
@@ -14,51 +14,54 @@ public class Horario {
 	private List<String> columnTitles;
 	private Map<String, Integer> ordemCampos;
 	private Salas salas;
+	private SaveState ss = new SaveState();
 
 	private Map<Metrica, Integer> metricas;
-	
+
 	/**
 	 * Construtor da classe Schedule.
 	 *
-//	 * @param table, Reader de tabela que fornece os dados para o horario.
+	 * // * @param table, Reader de tabela que fornece os dados para o horario.
 	 */
 
 	public Horario(String horarioFilePath) {
 		Reader dataHorarioDoCSV = new Reader(horarioFilePath);
 		columnTitles = dataHorarioDoCSV.getColumnTitles();
 		horario = dataHorarioDoCSV.getTableData();
-
+		ss.guardarHorario(csvFilePath, ordemCampos);
 		salas = new Salas(csvFilePath);
 	}
 
 	public void adicionarMetrica(Metrica metrica) {
+		ss.guardarMetricas(metrica);
 		metricas.put(metrica, calcularQualidade(metrica));
 	}
-	
+
 	private int calcularQualidade(Metrica metrica) {
-		List<String> formula = metrica.getComponentesFormula();   //Vai buscar os componetes da formula para o calculo da metrica
+		List<String> formula = metrica.getComponentesFormula(); // Vai buscar os componetes da formula para o calculo da
+																// metrica
 		List<String> atributo1 = defineAtributo(formula.get(0));
 		List<String> atributo2 = defineAtributo(formula.get(2));
 		List<String> contaInicial = new ArrayList<>();
-		if(atributo1.get(0).equals("horario") && atributo2.get(0).equals("horario")) {
+		if (atributo1.get(0).equals("horario") && atributo2.get(0).equals("horario")) {
 			contaInicial = calculoParcialHorario(atributo1, atributo2, formula.get(1));
-		}else if(atributo1.get(0).equals("sala") && atributo2.get(0).equals("horario")) {
+		} else if (atributo1.get(0).equals("sala") && atributo2.get(0).equals("horario")) {
 			contaInicial = calculoParcialHorarioSala(atributo2, atributo1, formula.get(1));
-		}else if(atributo1.get(0).equals("horario") && atributo2.get(0).equals("sala")) {
+		} else if (atributo1.get(0).equals("horario") && atributo2.get(0).equals("sala")) {
 			contaInicial = calculoParcialSalaHorario(atributo1, atributo2, formula.get(1));
 		}
-		if(formula.get(3) != null) {
+		if (formula.get(3) != null) {
 			int count = 0;
-			for(int i = 0; i < contaInicial.size(); i++) {
-				switch(formula.get(3)) {
+			for (int i = 0; i < contaInicial.size(); i++) {
+				switch (formula.get(3)) {
 				case "<":
-					if(Integer.parseInt(contaInicial.get(i)) < Integer.parseInt(formula.get(4))) {
+					if (Integer.parseInt(contaInicial.get(i)) < Integer.parseInt(formula.get(4))) {
 						count++;
 						metrica.adicionarAula(getHorario().get(i));
 					}
 					break;
 				case ">":
-					if(Integer.parseInt(contaInicial.get(i)) > Integer.parseInt(formula.get(4))) {
+					if (Integer.parseInt(contaInicial.get(i)) > Integer.parseInt(formula.get(4))) {
 						count++;
 						metrica.adicionarAula(getHorario().get(i));
 					}
@@ -68,15 +71,15 @@ public class Horario {
 				}
 			}
 			return count;
-		}else if(formula.get(1).equals("=") || formula.get(1).equals("!=")) {
+		} else if (formula.get(1).equals("=") || formula.get(1).equals("!=")) {
 			int count = 0;
 			for (int i = 0; i < contaInicial.size(); i++) {
-				if(contaInicial.get(i).equals("true")) {
+				if (contaInicial.get(i).equals("true")) {
 					count++;
 				}
 			}
 			return count;
-		}else {
+		} else {
 			int sum = 0;
 			for (int i = 0; i < contaInicial.size(); i++) {
 				sum += Integer.parseInt(contaInicial.get(i));
@@ -86,31 +89,44 @@ public class Horario {
 	}
 
 	/**
-	 * Devolve uma lista da informacao de um certo atributo/coluna, independentemente do ficheiro em que se localiza
+	 * Devolve uma lista da informacao de um certo atributo/coluna,
+	 * independentemente do ficheiro em que se localiza
 	 *
-//	 * @param coluna String da coluna que se quer obter informacao.
-//	 * @return Lista de strings representando os dados da coluna.
+	 * // * @param coluna String da coluna que se quer obter informacao. //
+	 * * @return Lista de strings representando os dados da coluna.
 	 */
 	private List<String> defineAtributo(String nomeAtributo) {
 		List<String> atributo = new ArrayList<>();
-		if(getColumnTitles().contains(nomeAtributo)) {   //Verifica se o atributo (que é um campo/coluna de um dos ficheiros(horario ou salas)) pertence ao horario
+		if (getColumnTitles().contains(nomeAtributo)) { // Verifica se o atributo (que é um campo/coluna de um dos
+														// ficheiros(horario ou salas)) pertence ao horario
 			atributo.add("horario");
-			for(int i = 0; i < getHorario().size(); i++) {
-				atributo.add(getHorario().get(i).get(ordemCampos.get(nomeAtributo)));   //Vai busacar uma lista com todo o conteudo do atributo
+			for (int i = 0; i < getHorario().size(); i++) {
+				atributo.add(getHorario().get(i).get(ordemCampos.get(nomeAtributo))); // Vai busacar uma lista com todo
+																						// o conteudo do atributo
 			}
-		}else if(getSalas().getColumnTitles().contains(nomeAtributo)){   //Verifica se o atributo pertence as salas
+		} else if (getSalas().getColumnTitles().contains(nomeAtributo)) { // Verifica se o atributo pertence as salas
 			atributo.add("sala");
-			for(int i = 0; i < getHorario().size(); i++) {
-				atributo.add(getSalas().getSalas().get(i).getCampo(getSalas().getColumnTitles().indexOf(nomeAtributo)));   //Vai busacar uma lista com todo o conteudo do atributo
+			for (int i = 0; i < getHorario().size(); i++) {
+				atributo.add(getSalas().getSalas().get(i).getCampo(getSalas().getColumnTitles().indexOf(nomeAtributo))); // Vai
+																															// busacar
+																															// uma
+																															// lista
+																															// com
+																															// todo
+																															// o
+																															// conteudo
+																															// do
+																															// atributo
 			}
 		}
 		return atributo;
 	}
-	
-	private List<String> calculoParcialHorario(List<String> atributoHoraio1, List<String> atributoHoraio2, String operador){
+
+	private List<String> calculoParcialHorario(List<String> atributoHoraio1, List<String> atributoHoraio2,
+			String operador) {
 		List<String> contaInicial = new ArrayList<>();
-		for(int i = 1; i < atributoHoraio1.size(); i++) {
-			switch(operador) {
+		for (int i = 1; i < atributoHoraio1.size(); i++) {
+			switch (operador) {
 //			case "-":
 //				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio1.get(i)) - Integer.parseInt(atributoHoraio2.get(i))));
 //				break;
@@ -122,54 +138,64 @@ public class Horario {
 		}
 		return contaInicial;
 	}
-	
-	private List<String> calculoParcialHorarioSala(List<String> atributoHoraio, List<String> atributoSala, String operador){
+
+	private List<String> calculoParcialHorarioSala(List<String> atributoHoraio, List<String> atributoSala,
+			String operador) {
 		List<String> contaInicial = new ArrayList<>();
-		for(int i = 1; i < atributoHoraio.size(); i++) {
-			switch(operador) {
+		for (int i = 1; i < atributoHoraio.size(); i++) {
+			switch (operador) {
 			case "*":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i)) * Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i))
+						* Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
 				break;
 			case "/":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i)) / Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i))
+						/ Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
 				break;
 			case "+":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i)) + Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i))
+						+ Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
 				break;
 			case "-":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i)) - Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoHoraio.get(i))
+						- Integer.parseInt(atributoSala.get(indexSalaAula(i)))));
 				break;
 			}
 		}
 		return contaInicial;
 	}
-	
-	private List<String> calculoParcialSalaHorario(List<String> atributoSala, List<String> atributoHoraio, String operador){
+
+	private List<String> calculoParcialSalaHorario(List<String> atributoSala, List<String> atributoHoraio,
+			String operador) {
 		List<String> contaInicial = new ArrayList<>();
-		for(int i = 1; i < atributoHoraio.size(); i++) {
-			switch(operador) {
+		for (int i = 1; i < atributoHoraio.size(); i++) {
+			switch (operador) {
 			case "*":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i))) * Integer.parseInt(atributoHoraio.get(i))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i)))
+						* Integer.parseInt(atributoHoraio.get(i))));
 				break;
 			case "/":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i))) / Integer.parseInt(atributoHoraio.get(i))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i)))
+						/ Integer.parseInt(atributoHoraio.get(i))));
 				break;
 			case "+":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i))) + Integer.parseInt(atributoHoraio.get(i))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i)))
+						+ Integer.parseInt(atributoHoraio.get(i))));
 				break;
 			case "-":
-				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i))) - Integer.parseInt(atributoHoraio.get(i))));
+				contaInicial.add(String.valueOf(Integer.parseInt(atributoSala.get(indexSalaAula(i)))
+						- Integer.parseInt(atributoHoraio.get(i))));
 				break;
 			}
 		}
 		return contaInicial;
 	}
-	
+
 	private int indexSalaAula(int index) {
 		int indexSalaHorario = 0;
 		int i = 1;
 		for (Map.Entry<String, Integer> entry : ordemCampos.entrySet()) {
-			if(ordemCampos.entrySet().size() == i) {
+			if (ordemCampos.entrySet().size() == i) {
 				indexSalaHorario = entry.getValue();
 			}
 			i++;
---
+++ ProjetoES/src/main/java/LETI_GrupoF/ProjetoES/SaveState.java
@@ -0,0 +1,140 @@
+package LETI_GrupoF.ProjetoES;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SaveState {
+	private String SaveStateFilePath = "SaveState.txt";
+
+	/**
+	 * Este método tem como finalidade armazenar um horário, que possibilita ao
+	 * utilizador a sua reutilização caso este queira retomar a sua sessão anterior.
+	 *
+	 * @param csvFilePath Indica qual o horario usado pelo utilizador na sessao
+	 * @param ordemCampos Um mapa que define a ordem e a posicao dos campos no
+	 *                    horario. Cada chave representa um campo e o valor
+	 *                    associado indica a posicao do mesmo.
+	 */
+
+	void guardarHorario(String csvFilePath, Map<String, Integer> ordemCampos) {
+
+		List<Map.Entry<String, Integer>> entryList = new ArrayList<>(ordemCampos.entrySet());
+		entryList.sort(Comparator.comparing(Map.Entry::getValue));
+		try {
+			FileWriter file = new FileWriter(SaveStateFilePath);
+			PrintWriter writer = new PrintWriter(file);
+
+			writer.println(csvFilePath);
+
+			for (Map.Entry<String, Integer> entry : ordemCampos.entrySet()) {
+				writer.println(entry.getKey() + ":" + entry.getValue());
+			}
+			writer.println("FOC");
+			writer.close();
+
+		} catch (IOException e) {
+			System.err.println("An error occurred while writing to the file: " + e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Este metodo tem como objetivo armazenar as metricas, preservando as formulas
+	 * personalizadas introduzidas pelo utilizador. Permitindo assim restaurar o
+	 * estado anterior das formulas caso o utilizador pretenda recuperar a sessao
+	 * anterior
+	 *
+	 * @param formula A formula criada pelo utilizador que sera guardada. Permite
+	 *                recuperar a formula caso o utilizador retome a sessão.
+	 */
+
+	void guardarMetricas(Metrica metrica) {
+
+		try {
+			FileWriter file = new FileWriter(SaveStateFilePath);
+			PrintWriter writer = new PrintWriter(file);
+
+			writer.println(metrica.getFormula());
+			writer.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Recupera o horario anterior, permitindo que o utilizador retome a sua sessao
+	 * anterior.
+	 *
+	 * @return Um objeto do tipo Horario representando o horario previamente
+	 *         armazenado. Retorna null se não houver um horario anteriormente
+	 *         armazenado.
+	 */
+
+	Horario RecuperarHorarioAntigo() {
+
+		String FilePath = null;
+		Map<String, Integer> oCampos = new HashMap<>();
+		List<Metrica> metricas = new ArrayList<>();
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(SaveStateFilePath));
+
+			FilePath = reader.readLine();
+
+			String linha = reader.readLine();
+			while (!linha.trim().equals("FOC")) {
+				String[] partes = linha.split(":");
+				oCampos.put(partes[0], Integer.parseInt(partes[1]));
+			}
+
+			while (linha != null) {
+				Metrica novaMetrica = new Metrica(linha.trim());
+				metricas.add(novaMetrica);
+			}
+
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+
+		Horario horarioRecuperado = new Horario(FilePath);
+		horarioRecuperado.setOrdemCampos(oCampos);
+
+		for (int i = 0; i != metricas.size(); i++) {
+			horarioRecuperado.adicionarMetrica(metricas.get(i));
+		}
+
+		return horarioRecuperado;
+
+	}
+
+	/**
+	 * Limpa o conteudo do arquivo SaveState.txt, removendo todas as informacoes
+	 * previamente armazenadas. Esta funcao e utilizada quando o utilizador opta por
+	 * nao retomar a sessao anterior.
+	 *
+	 */
+	void LimparSaveState() {
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter(SaveStateFilePath, false);
+			fileWriter.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
